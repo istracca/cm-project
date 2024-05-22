a42a375e26d023ddc0ba4a44f53c25160f06927d
istracca <153355919+istracca@users.noreply.github.com>
2024-05-22 14:42:06 +0200

    Add files via upload

--- prova_matrice.m
+++ prova_matrice.m
@@ -1,39 +1,36 @@
-[A, y] = create_matrix_from_dimacs('netgen-3000-1-1-a-b-ns.dmx', 2312);
-
+[A, y] = create_matrix_from_dimacs('graph_instances/net12_8_1.dmx', 1);
+prova = 1
 % Error threshold
 eps = 1e-6;
 
-Q = y / norm(y);
-H = [];
-[Q, H] = lanczos(Q, H, A, 2);
+% Misura tempo per la prima sezione
+tic;
 
-% Compute c1 and s1 to perform first Givens rotation
-c1 = H(1,1)/sqrt(H(1,1)^2 + H(2,1)^2);
-s1 = H(2,1)/sqrt(H(1,1)^2 + H(2,1)^2);
-Q_h = [c1 -s1; s1 c1];
-R_h = Q_h' * H;
+[custom_x, custom_flag, custom_relres, custom_iter, custom_resvec] = custom_minres(A, y, 1e-6, 10000);
+custom_relative_resvec = custom_resvec/norm(y);
+
+elapsedTime1 = toc;
+disp(['Tempo per la creazione della matrice: ', num2str(elapsedTime1), ' secondi']);
 
 
-for i=3:10000
-    % Expand Lanczos process
-    [Q, H] = lanczos(Q, H, A, i);
+% Misura tempo per la seconda sezione
+tic;
+[x, flag, relres, iter, resvec] = minres(A, y, 1e-6, 10000);
+relative_resvec = resvec/norm(y);
+%relative_resvec = relative_resvec(2:end);
 
-    % Calculate QR decomposition
-    [Q_h, R_h] = qr_iterative(H, Q_h, R_h, i);
-    
-    % Solving the linear system
-    c = zeros(i, 1);
-    c(1) = norm(y);
-    Q_h_c = (Q_h')*c;
-    R_h0 = R_h(1:i-1,:);
-    z = R_h0 \ Q_h_c(1:i-1);
+elapsedTime2 = toc;
+disp(['Tempo per il calcolo della matrice al quadrato: ', num2str(elapsedTime2), ' secondi']);
 
-    % Check for convergence
-    err = R_h0*z - Q_h_c(1:i-1);
-    err(i) = Q_h_c(i);
-    norm(err(i))
-    if norm(err(i)) < eps
-        fprintf("Number of iterations: %d\n", i);
-        break;
-    end
-end
+% Grafico dell'evoluzione dei residui relativi
+figure;
+semilogy(custom_relative_resvec, 'b', 'LineWidth', 1.5);
+hold on;
+semilogy(relative_resvec, 'r', 'LineWidth', 1.5);
+hold off;
+
+xlabel('Iterazioni');
+ylabel('Residuo relativo');
+legend('Custom MINRES', 'Matlab MINRES');
+title('Evoluzione dei residui relativi');
+grid on;
